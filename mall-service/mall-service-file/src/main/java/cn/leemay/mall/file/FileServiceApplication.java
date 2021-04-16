63f6ab0f1d7da5df3ff62057146f2ea6c92ed153
wj <xwj1024@yeah.net>
2021-04-16 11:11:55 +0800

    swagger

--- mall-service/mall-service-file/src/main/java/cn/leemay/mall/file/FileServiceApplication.java
+++ mall-service/mall-service-file/src/main/java/cn/leemay/mall/file/FileServiceApplication.java
@@ -1,5 +1,6 @@
 package cn.leemay.mall.file;
 
+import cn.leemay.mall.common.base.anno.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @create 2021/4/14
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableSwagger
 public class FileServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileServiceApplication.class, args);
