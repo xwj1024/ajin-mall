be92cfb9ab72fa4d3866459cd1a14af2425bcb61
wj <xwj1024@yeah.net>
2021-05-26 00:49:46 +0800

    1

---
+++ mall-service/mall-service-tool/src/main/java/cn/leemay/mall/tool/controller/ImportController.java
@@ -0,0 +1,17 @@
+package cn.leemay.mall.tool.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ajin
+ * @since 2021-05-26
+ */
+@RestController
+@RequestMapping("/tool/import")
+@Api(tags = "导入")
+@CrossOrigin
+public class ImportController {
+}
