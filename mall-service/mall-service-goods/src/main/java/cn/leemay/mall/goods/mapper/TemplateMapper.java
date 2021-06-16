adc4227a9b885a3adf337669feeb263a5dba541d
wj <xwj1024@yeah.net>
2021-06-16 17:48:23 +0800

    a

--- mall-service/mall-service-goods/src/main/java/cn/leemay/mall/goods/mapper/TemplateMapper.java
+++ mall-service/mall-service-goods/src/main/java/cn/leemay/mall/goods/mapper/TemplateMapper.java
@@ -3,7 +3,6 @@ package cn.leemay.mall.goods.mapper;
 import cn.leemay.mall.goods.entity.Template;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Repository;
  * @author Ajin
  * @since 2021-04-13
  */
-@Repository
 @Mapper
 public interface TemplateMapper extends BaseMapper<Template> {
 
