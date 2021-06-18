a2c6195fc1ec3b85e22016bb6662d55ac269366e
wj <xwj1024@yeah.net>
2021-06-18 23:47:54 +0800

    1

--- mall-service/mall-service-goods/src/main/java/cn/leemay/mall/goods/GoodsServiceApplication.java
+++ mall-service/mall-service-goods/src/main/java/cn/leemay/mall/goods/GoodsServiceApplication.java
@@ -3,7 +3,6 @@ package cn.leemay.mall.goods;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ImportResource;
 
 
 /**
