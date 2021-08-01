37e39d18e8abf936f6438d9a8aa1813ee8b766db
ajin <xwj1024@yeah.net>
2021-08-02 00:25:02 +0800

    全局异常处理修改

--- mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/handler/ExceptionHandlers.java
+++ mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/handler/ExceptionHandlers.java
@@ -5,17 +5,14 @@ import cn.leemay.mall.common.base.exception.SysException;
 import cn.leemay.mall.common.base.result.BaseResult;
 import cn.leemay.mall.common.base.result.ResultCode;
 import lombok.extern.slf4j.Slf4j;
-import org.omg.CORBA.SystemException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author Ajin
@@ -32,7 +29,7 @@ public class ExceptionHandlers {
     }
 
     @ExceptionHandler(SysException.class)
-    public BaseResult<String> handleSysException(SystemException e) {
+    public BaseResult<String> handleSysException(SysException e) {
         log.error(e.getMessage());
         return new BaseResult<>(ResultCode.ERR, e.getMessage());
     }
@@ -41,8 +38,11 @@ public class ExceptionHandlers {
     public BaseResult<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
         if (bindingResult.hasErrors()) {
-            Map<String, String> errMap = bindingResult.getFieldErrors().stream()
-                    .collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));
+            Map<String, String> errMap = new HashMap<>(2);
+            bindingResult.getFieldErrors().forEach(fieldError ->
+                    errMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
+            /*Map<String, String> errMap = bindingResult.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));*/
             log.error(e.getMessage());
             return new BaseResult<>(ResultCode.ERR, e.getMessage(), errMap);
         }
