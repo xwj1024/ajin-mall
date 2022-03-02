46865b9c59b2a1bac0e50f181414c28390b81a6f
xwj <xwj1024@yeah.net>
2022-03-02 16:00:04 +0800

    page

--- mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/page/PageForm.java
+++ mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/page/PageForm.java
@@ -3,6 +3,7 @@ package cn.leemay.mall.common.base.page;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -11,14 +12,14 @@ import java.io.Serializable;
  */
 @Data
 public class PageForm implements Serializable {
-
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("当前页")
-    protected Integer index;
+    protected Integer pageIndex;
 
     @ApiModelProperty("每页条数")
-    protected Integer size;
+    protected Integer pageSize;
 
     @ApiModelProperty("关键字")
     protected String keyword;
