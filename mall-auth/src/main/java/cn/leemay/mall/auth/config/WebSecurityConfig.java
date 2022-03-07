95e8a1dd0481ee326cce6386aaab995476e16058
xwj <xwj1024@yeah.net>
2022-03-07 09:30:15 +0800

    auth

--- mall-auth/src/main/java/cn/leemay/mall/auth/config/WebSecurityConfig.java
+++ mall-auth/src/main/java/cn/leemay/mall/auth/config/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package cn.leemay.mall.auth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
 
 /**
  * @author Ajin
@@ -15,15 +20,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Resource
+    private UserDetailsService userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/oauth/**").permitAll()
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/oauth/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().permitAll();
     }
