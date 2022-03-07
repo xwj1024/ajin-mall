88a18ed7daf604fd80e7da3bda5077a58badde51
xwj <xwj1024@yeah.net>
2022-03-07 09:34:34 +0800

    repeat

--- mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/interceptor/SameUriDataInterceptor.java
+++ mall-common/mall-common-base/src/main/java/cn/leemay/mall/common/base/interceptor/SameUriDataInterceptor.java
@@ -1,6 +1,5 @@
 package cn.leemay.mall.common.base.interceptor;
 
-import cn.leemay.mall.common.base.interceptor.RepeatSubmitInterceptor;
 import cn.leemay.mall.common.base.util.HttpUtils;
 import cn.leemay.mall.common.base.wrapper.RepeatRequestWrapper;
 import com.alibaba.fastjson.JSON;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,15 +29,16 @@ import java.util.concurrent.TimeUnit;
 @Component
 @ConfigurationProperties(prefix = "repeat")
 public class SameUriDataInterceptor extends RepeatSubmitInterceptor implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    private String repeatParam = "repeatParam";
+    private static final String REPEAT_PARAM = "repeatParam";
 
-    private String repeatTime = "repeatTime";
+    private static final String REPEAT_TIME = "repeatTime";
 
-    private String repeatKey = "repeatKey";
+    private static final String REPEAT_KEY = "repeatKey";
 
-    private String header = "Authorization";
+    private static final String HEADER = "Authorization";
 
     private int intervalTime = 3000;
 
@@ -60,21 +61,21 @@ public class SameUriDataInterceptor extends RepeatSubmitInterceptor implements S
 
         // 获取本次请求参数及系统时间
         Map<String, Object> nowDataMap = new HashMap<>(2);
-        nowDataMap.put(repeatParam, JSON.toJSONString(nowParam));
-        nowDataMap.put(repeatTime, System.currentTimeMillis());
+        nowDataMap.put(REPEAT_PARAM, JSON.toJSONString(nowParam));
+        nowDataMap.put(REPEAT_TIME, System.currentTimeMillis());
 
         // 请求地址（作为存放cache的key值）
         String uri = request.getRequestURI();
 
         // 唯一值（没有请求头则使用请求地址）
-        String submitKey = request.getHeader(header);
+        String submitKey = request.getHeader(HEADER);
         if (StringUtils.isEmpty(submitKey)) {
             submitKey = uri;
         }
 
         // 唯一标识（指定key + 请求头）
-        String repeatSubmitKey = repeatKey + submitKey;
-        Object obj = stringRedisTemplate.opsForValue().get(repeatSubmitKey);
+        String repeatSubmitKey = REPEAT_KEY + submitKey;
+        Object obj             = stringRedisTemplate.opsForValue().get(repeatSubmitKey);
         if (obj != null) {
             Map<String, Object> oldDataMap = JSON.parseObject(obj.toString(), Map.class);
             if (oldDataMap.containsKey(uri)) {
@@ -94,8 +95,8 @@ public class SameUriDataInterceptor extends RepeatSubmitInterceptor implements S
      * 判断参数是否相同
      */
     private boolean compareParam(Map<String, Object> nowMap, Map<String, Object> preMap) {
-        String nowParam = (String) nowMap.get(repeatParam);
-        String preParam = (String) preMap.get(repeatParam);
+        String nowParam = (String) nowMap.get(REPEAT_PARAM);
+        String preParam = (String) preMap.get(REPEAT_PARAM);
         return nowParam.equals(preParam);
     }
 
@@ -103,8 +104,8 @@ public class SameUriDataInterceptor extends RepeatSubmitInterceptor implements S
      * 判断两次间隔时间
      */
     private boolean compareTime(Map<String, Object> nowMap, Map<String, Object> preMap) {
-        long nowTime = (Long) nowMap.get(repeatTime);
-        long preTime = (Long) preMap.get(repeatTime);
+        long nowTime = (Long) nowMap.get(REPEAT_TIME);
+        long preTime = (Long) preMap.get(REPEAT_TIME);
         return nowTime - preTime < intervalTime;
     }
 }
