973ee6c0626a3e07392f9d973ccb636f8a5b2b1b
xwj <xwj1024@yeah.net>
2022-03-09 11:09:57 +0800

    cascade delete

---
+++ mall-common/mall-common-data/src/main/java/cn/leemay/mall/common/data/anno/EnableCascadeDelete.java
@@ -0,0 +1,16 @@
+package cn.leemay.mall.common.data.anno;
+
+import cn.leemay.mall.common.data.handler.CascadeDeleteHandler;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Ajin
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({CascadeDeleteHandler.class})
+public @interface EnableCascadeDelete {
+}
