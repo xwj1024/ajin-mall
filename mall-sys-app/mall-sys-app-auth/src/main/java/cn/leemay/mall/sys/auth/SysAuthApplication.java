8670dae8a0dfcb955bcb7f41c1611153a8563fc5
xwj <xwj1024@yeah.net>
2022-03-14 17:33:02 +0800

    3..

--- mall-sys-app/mall-sys-app-auth/src/main/java/cn/leemay/mall/sys/auth/SysAuthApplication.java
+++ mall-sys-app/mall-sys-app-auth/src/main/java/cn/leemay/mall/sys/auth/SysAuthApplication.java
@@ -1,6 +1,6 @@
 package cn.leemay.mall.sys.auth;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,8 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author Ajin
  * @since 2021-05-16
  */
-@EnableDubbo
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class SysAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(SysAuthApplication.class, args);
