4db25fece7b6a4a507f444f586bada96b806ab91
xwj <xwj1024@yeah.net>
2022-03-21 17:38:30 +0800

    sys operate log

--- mall-common/mall-common-base/src/main/java/ajin/mall/common/base/aspect/RecordSysOperateLogAspect.java
+++ mall-common/mall-common-base/src/main/java/ajin/mall/common/base/aspect/RecordSysOperateLogAspect.java
@@ -2,10 +2,7 @@ package ajin.mall.common.base.aspect;
 
 import ajin.mall.common.base.anno.RecordSysOperateLog;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,30 +21,16 @@ public class RecordSysOperateLogAspect {
 
 
     /**
-     * 处理完请求后执行
-     *
-     * @param joinPoint  切点
-     * @param rsol    日志注解
-     * @param jsonResult 返回值
-     */
-    @AfterReturning(pointcut = "@annotation(rsol)", returning = "jsonResult")
-    public void doAfterReturning(JoinPoint joinPoint, RecordSysOperateLog rsol, Object jsonResult) {
-        handleLog(joinPoint, rsol, null, jsonResult);
-    }
-
-    /**
-     * 拦截异常操作
+     * 处理请求前执行
      *
      * @param joinPoint 切点
-     * @param rsol   日志注解
-     * @param e         异常
      */
-    @AfterThrowing(value = "@annotation(rsol)", throwing = "e")
-    public void doAfterThrowing(JoinPoint joinPoint, RecordSysOperateLog rsol, Exception e) {
-        handleLog(joinPoint, rsol, e, null);
+    @Before("sysOperateLogPointCut()")
+    public void doBefore(JoinPoint joinPoint) {
+        handleLog(joinPoint);
     }
 
-    private void handleLog(final JoinPoint joinPoint, RecordSysOperateLog rsol, final Exception e, Object jsonResult) {
+    private void handleLog(final JoinPoint joinPoint) {
         // todo
     }
 
