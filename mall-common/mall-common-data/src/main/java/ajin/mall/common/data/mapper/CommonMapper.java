1ac51e162debfdec7b698690a9e0a524de728aa6
ajin <xwj1024@yeah.net>
2022-03-24 21:10:29 +0800

    CommonMapper

--- mall-common/mall-common-data/src/main/java/ajin/mall/common/data/mapper/CommonMapper.java
+++ mall-common/mall-common-data/src/main/java/ajin/mall/common/data/mapper/CommonMapper.java
@@ -21,7 +21,7 @@ public interface CommonMapper {
      * @param linkedFieldName 字段
      * @return 结果数量
      */
-    @Select("select count(*) from `${linkedTableName}` where `${linkedFieldName}` = #{arg} and is_delete = 0")
+    @Select("select count(*) from `${linkedTableName}` where `${linkedFieldName}` = #{arg} and `is_delete` = 0")
     int count(@Param("arg") Long arg, @Param("linkedTableName") String linkedTableName, @Param("linkedFieldName") String linkedFieldName);
 
     /**
@@ -32,7 +32,7 @@ public interface CommonMapper {
      * @param linkedFieldName 字段
      * @return 删除结果
      */
-    @Update("update `${linkedTableName}` set is_delete = 1 where `${linkedFieldName}` = #{arg}")
+    @Update("update `${linkedTableName}` set `is_delete` = 1 where `${linkedFieldName}` = #{arg}")
     int delete(@Param("arg") Long arg, @Param("linkedTableName") String linkedTableName, @Param("linkedFieldName") String linkedFieldName);
 
     /**
