216623c4d788d9d470fb49c93e134388800b3de4
ajin <xwj1024@yeah.net>
2022-03-27 21:46:48 +0800

    gateway 没有 context path

---
+++ mall-sys-app/mall-sys-app-auth/src/main/java/ajin/mall/sys/auth/config/AuthorizationServerConfig.java
@@ -0,0 +1,22 @@
+//package ajin.mall.sys.auth.config;
+//
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+//
+//import javax.annotation.Resource;
+//import javax.sql.DataSource;
+//
+///**
+// * @author Ajin
+// * @since 2022-3-27
+// */
+//public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+//
+//    @Resource
+//    private DataSource dataSource;
+//
+//    @Resource
+//    private JwtAccessTokenConverter jwtAccessTokenConverter;
+//
+//
+//}
