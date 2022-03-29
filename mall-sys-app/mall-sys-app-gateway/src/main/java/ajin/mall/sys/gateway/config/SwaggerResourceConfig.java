a9bc34231a981004361c7b0ba9c2ade6e8a5ada4
ajin <xwj1024@yeah.net>
2022-03-29 22:29:58 +0800

    ？？？

--- mall-sys-app/mall-sys-app-gateway/src/main/java/ajin/mall/sys/gateway/config/SwaggerResourceConfig.java
+++ mall-sys-app/mall-sys-app-gateway/src/main/java/ajin/mall/sys/gateway/config/SwaggerResourceConfig.java
@@ -38,7 +38,7 @@ public class SwaggerResourceConfig implements SwaggerResourcesProvider {
                     .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
                     .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
                             predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
-                                    .replace("**", "/v3/api-docs"))));
+                                    .replace("**", "v3/api-docs"))));
         });
 
         return resources;
