02510ad9a9fda77e938d6578b9877c2dbf884de6
xwj <xwj1024@yeah.net>
2022-03-29 16:39:29 +0800

    数据库加密

--- mall-common/mall-common-data/src/main/java/ajin/mall/common/data/config/MybatisPlusConfig.java
+++ mall-common/mall-common-data/src/main/java/ajin/mall/common/data/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package ajin.mall.common.data.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,7 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
-
 }
