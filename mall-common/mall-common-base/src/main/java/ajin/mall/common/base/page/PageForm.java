3f633c53155dca0ee67cb772500b55b631463642
xwj <xwj1024@yeah.net>
2022-03-29 11:22:59 +0800

    查询列表排序规则

--- mall-common/mall-common-base/src/main/java/ajin/mall/common/base/page/PageForm.java
+++ mall-common/mall-common-base/src/main/java/ajin/mall/common/base/page/PageForm.java
@@ -13,18 +13,18 @@ import java.io.Serializable;
 public class PageForm implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("当前页")
-    protected Integer pageIndex;
+    @ApiModelProperty("当前页，默认第1页")
+    protected Integer pageIndex = 1;
 
-    @ApiModelProperty("每页条数")
-    protected Integer pageSize;
+    @ApiModelProperty("每页条数，默认10条")
+    protected Integer pageSize = 10;
 
     @ApiModelProperty("关键字")
     protected String keyword;
 
-    @ApiModelProperty("排序根据")
+    @ApiModelProperty("排序根据：数据库字段名")
     protected String orderBy;
 
-    @ApiModelProperty("排序规则：asc/desc")
+    @ApiModelProperty("排序规则：升序asc/降序desc")
     protected String sortType;
 }
