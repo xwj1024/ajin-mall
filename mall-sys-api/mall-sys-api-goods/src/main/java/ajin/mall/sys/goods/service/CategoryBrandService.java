9dfb221a9bf67662703d2f7ea3932bfa390fc2fa
xwj <xwj1024@yeah.net>
2022-03-30 17:05:23 +0800

    biz

--- mall-sys-api/mall-sys-api-goods/src/main/java/ajin/mall/sys/goods/service/CategoryBrandService.java
+++ mall-sys-api/mall-sys-api-goods/src/main/java/ajin/mall/sys/goods/service/CategoryBrandService.java
@@ -16,12 +16,32 @@ import ajin.mall.sys.goods.view.CategoryBrandView;
  */
 public interface CategoryBrandService {
 
+    /**
+     * 添加商品分类，商品品牌
+     *
+     * @param categoryBrandAddForm 添加信息
+     */
     void add(CategoryBrandAddForm categoryBrandAddForm);
 
+    /**
+     * 删除商品分类，商品品牌
+     *
+     * @param id id
+     */
     void delete(Long id);
 
+    /**
+     * 修改商品分类，商品品牌
+     *
+     * @param categoryBrandUpdateForm 修改信息
+     */
     void update(CategoryBrandUpdateForm categoryBrandUpdateForm);
 
+    /**
+     * 根据id查询商品分类，商品品牌
+     * @param id id
+     * @return
+     */
     CategoryBrandView get(Long id);
 
     ResultPage<CategoryBrandView> list(CategoryBrandListForm categoryBrandListForm);
