e6c884a7f5f77b162025e4c2915a4deccb9f77d7
ajin <xwj1024@yeah.net>
2022-04-07 22:32:37 +0800

    member

--- mall-sys-app/mall-sys-app-member/src/main/java/ajin/mall/sys/member/service/impl/MemberServiceImpl.java
+++ mall-sys-app/mall-sys-app-member/src/main/java/ajin/mall/sys/member/service/impl/MemberServiceImpl.java
@@ -1,6 +1,11 @@
 package ajin.mall.sys.member.service.impl;
 
+import ajin.mall.common.base.result.ResultPage;
+import ajin.mall.sys.member.form.MemberAddForm;
+import ajin.mall.sys.member.form.MemberListForm;
+import ajin.mall.sys.member.form.MemberUpdateForm;
 import ajin.mall.sys.member.service.MemberService;
+import ajin.mall.sys.member.view.MemberView;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +13,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MemberServiceImpl implements MemberService {
+    @Override
+    public void add(MemberAddForm memberAddForm) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void update(MemberUpdateForm memberUpdateForm) {
+
+    }
+
+    @Override
+    public MemberView get(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResultPage<MemberView> list(MemberListForm memberListForm) {
+        return null;
+    }
 }
